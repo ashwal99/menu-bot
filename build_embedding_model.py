b5dd51bb682aed837f61b8f7a3d01bc118435b32
abhinavssram <abhinavssram@gmail.com>
2023-07-07 23:23:35 +0530

    add changes for embedding models

---
+++ build_embedding_model.py
@@ -0,0 +1,22 @@
+import os
+from sentence_transformers import SentenceTransformer
+
+model_name = 'sentence-transformers/multi-qa-mpnet-base-dot-v1'
+model_path = 'menu-bot/ml_models/multi-qa-mpnet-base-dot-v1'
+
+def get_embedding_model():
+    if not os.path.exists(model_path):
+        # Download the model only if it doesn't exist
+        print("Downloading sentenceTransformer model....")
+        model = SentenceTransformer(model_name)
+        model.save(model_path)
+        print("sentenceTransformer model saved....")
+    else:
+        # Load the model from the local file system
+        print("Load sentenceTransformer model....")
+        model = SentenceTransformer(model_path)
+    
+    return model
+
+# if __name__ == "__main__":
+#     print(create_embedding_model())
