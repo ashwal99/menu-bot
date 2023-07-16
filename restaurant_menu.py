b43a35a73466bb2b913c76ee21a3999f296b046d
abhinavssram <abhinavssram@gmail.com>
2023-07-16 12:36:12 +0530

    update menu items

--- restaurant_menu.py
+++ restaurant_menu.py
@@ -1,15 +1,15 @@
 restaurant_menu = [
     "Margherita : Classic pizza with tomato sauce and mozzarella cheese : Veg : Veg Pizza : #vegan, #chefSpecial",
-    "Pepperoni : Pizza with tomato sauce, mozzarella, and pepperoni : Non-veg : NV Pizza : #spicy, #recommended",
+    "Margherita Extra : Margherita pizza with extra cheese and basil : Veg : Veg Pizza : #wantToRepeat",
+    "Pepperoni : Pizza with tomato sauce, mozzarella, and pepperoni : Non-veg : Non-Veg Pizza : #spicy, #recommended",
+    "BBQ Chicken : Pizza with barbecue sauce, chicken, and onions : Non-veg : Non-Veg Pizza : #specialtyChicken, #mealFor2",
+    "Hawaiian Pizza : Pizza with tomato sauce, mozzarella, ham, and pineapple : Non-veg : NV Pizza : #new, #recommended",
     "Caesar Salad : Romaine lettuce, croutons, Parmesan cheese, Caesar dressing : Veg : Specialty : #dairyFree, #wantToRepeat",
-    "BBQ Chicken : Pizza with barbecue sauce, chicken, and onions : Non-veg : NV Pizza : #specialtyChicken, #mealFor2",
-    "Garlic Bread : Sliced baguette with garlic butter and Parmesan : Veg : Sides : #new, #sides",
+    "Caprese Salad : Fresh mozzarella, tomato, and basil drizzled with balsamic : Veg : Specialty : #wantToRepeat, #dairyFree",
+    "Greek Salad : Lettuce, tomato, cucumber, feta cheese, and Kalamata olives : Veg : Specialty : #wantToRepeat, #dairyFree, #glutenFree",
+    "Tandoori Chicken : Chicken marinated in Indian spices and grilled : Non-veg : Specialty : #spicy, #recommended, #new",
     "Chicken Alfredo : Fettuccine with creamy Alfredo sauce and chicken : Non-veg : Meals and combos : #containsDairy, #mealFor2",
     "Vegan Burger : Plant-based burger patty with lettuce, tomato, and onion : Veg : Meals and combos : #guiltFree, #vegan, #mealFor2",
-    "Tandoori Chicken : Chicken marinated in Indian spices and grilled : Non-veg : Specialty : #spicy, #recommended, #new",
-    "Caprese Salad : Fresh mozzarella, tomato, and basil drizzled with balsamic : Veg : Specialty : #wantToRepeat, #dairyFree",
-    "Hawaiian Pizza : Pizza with tomato sauce, mozzarella, ham, and pineapple : Non-veg : NV Pizza : #new, #recommended",
-    "Margherita Extra : Margherita pizza with extra cheese and basil : Veg : Veg Pizza : #wantToRepeat",
+    "Garlic Bread : Sliced baguette with garlic butter and Parmesan : Veg : Sides : #new, #sides",
     "Buffalo Wings : Deep-fried chicken wings served with hot sauce and blue cheese : Non-veg : Sides : #spicy, #sides,#specialtyChicken, #new",
-    "Greek Salad : Lettuce, tomato, cucumber, feta cheese, and Kalamata olives : Veg : Specialty : #wantToRepeat, #dairyFree, #glutenFree"
 ]
